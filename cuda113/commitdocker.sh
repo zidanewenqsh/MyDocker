c74b66202461abfee5b5651802679028fef31b4d
zidanewenqsh <53895359+zidanewenqsh@users.noreply.github.com>
2025-03-06 15:54:21 +0800

    feat:nvidia_cuda cu113镜像打包了java
    - nvidia_cuda cu113镜像打包了java和Maven
    - nvidia_cuda cu113的docker-compose
    - swag这个可以忽略，本来想用swig，不过留着吧

--- cuda113/commitdocker.sh
+++ cuda113/commitdocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 image='registry.cn-hangzhou.aliyuncs.com/phzn/nvidia_cuda'
-version="open3d-pcl-grpc-trtpy-cu113-cudnn8-devel-ubuntu18-v3.2.5"
-ImageId='51ae6e55eec3'
-commitcmd="docker commit -a \"wen\" -m \"open3d\" ${ImageId} ${image}:${version}"
+version="java-open3d-pcl-grpc-trtpy-cu113-cudnn8-devel-ubuntu18-v3.3.1"
+ImageId='0d86c608adf4'
+commitcmd="docker commit -a \"wen\" -m \"java\" ${ImageId} ${image}:${version}"
 echo $commitcmd
 eval "$commitcmd"
--- cuda113/pushdocker.sh
+++ cuda113/pushdocker.sh
@@ -2,8 +2,8 @@
 prefix="registry.cn-hangzhou.aliyuncs.com/phzn"
 image='nvidia_cuda'
 # version="pcl-grpc-trtpy-cu113-cudnn8-devel-ubuntu18-v3.2.4"
-version="open3d-pcl-grpc-trtpy-cu113-cudnn8-devel-ubuntu18-v3.2.5"
-ImageId='37c22e09d0cc'
+version="java-open3d-pcl-grpc-trtpy-cu113-cudnn8-devel-ubuntu18-v3.3.1"
+ImageId='1819cfb7af'
 tagcmd="docker tag ${ImageId} ${prefix}/${image}:${version}"
 pushcmd="docker push ${prefix}/${image}:${version}"
 echo $tagcmd
